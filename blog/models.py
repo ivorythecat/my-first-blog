0901702520c0b472a4b037fffd15b626222c2a31
ivorythecat <ivoryhasastar@gmail.com>
2018-12-12 20:44:46 +0900

    reset my Django

--- blog/models.py
+++ blog/models.py
@@ -3,18 +3,15 @@ from django.db import models
 from django.utils import timezone
 
 
-# Create your models here.
-
-# Declare Object 'Post'
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    published_data = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.published_data = timezone.now()
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
